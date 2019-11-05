de5420c4e0719ddcb7db678792c2571a3ecb83cf
Constant <ukosname@mail.ru>
2019-11-05 23:50:41 +0300

    iOS UI fix for build

--- src/iOS/Presentation/Views/MainView.cs
+++ src/iOS/Presentation/Views/MainView.cs
@@ -1,18 +1,25 @@
 ﻿using MvvmCross.Binding.BindingContext;
-using MvvmCross.Platforms.Ios.Presenters.Attributes;
-using MvvmCross.Platforms.Ios.Views;
 using PrankChat.Mobile.Core.Presentation.ViewModels;
 
 namespace PrankChat.Mobile.iOS.Presentation.Views
 {
-    [MvxRootPresentation(WrapInNavigationController = true)]
-    public partial class MainView : MvxViewController
+    public partial class MainView : BaseView
     {
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
             var set = this.CreateBindingSet<MainView, MainViewModel>();
+            //set.Bind(TextField).To(vm => vm.Text);
+            //set.Bind(Button).To(vm => vm.ResetTextCommand);
             set.Apply();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
         }
     }
 }
+
