6d6a2b8b43990181f3f073010d05e73852a8aa23
Anton Tereshko <formation7@outlook.com>
2019-11-15 17:04:11 +0300

    add back button

--- src/iOS/Presentation/Views/NotificationView/NotificationView.cs
+++ src/iOS/Presentation/Views/NotificationView/NotificationView.cs
@@ -1,12 +1,26 @@
-﻿using MvvmCross.Platforms.Ios.Presenters.Attributes;
+﻿using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
 using PrankChat.Mobile.Core.Presentation.ViewModels;
 using PrankChat.Mobile.iOS.Presentation.Views.Base;
+using UIKit;
 
 namespace PrankChat.Mobile.iOS.Presentation.Views.NotificationView
 {
-    [MvxRootPresentation]
-    public partial class NotificationView : BaseView<NotificationViewModel>
+	[MvxModalPresentation(WrapInNavigationController = true)]
+	public partial class NotificationView : BaseView<NotificationViewModel>
     {
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			SetupBinding();
+		}
+
+		private void SetupBinding()
+		{
+			var set = this.CreateBindingSet<NotificationView, NotificationViewModel>();
+			set.Bind(backButton).To(vm => vm.GoBackCommand);
+			set.Apply();
+		}
 	}
 }
 
--- src/iOS/Presentation/Views/NotificationView/NotificationView.designer.cs
+++ src/iOS/Presentation/Views/NotificationView/NotificationView.designer.cs
@@ -1,21 +1,26 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace PrankChat.Mobile.iOS.Presentation.Views.NotificationView
 {
-    [Register("NotificationView")]
-    partial class NotificationView
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("NotificationView")]
+	partial class NotificationView
+	{
+		[Outlet]
+		UIKit.UIButton backButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (backButton != null) {
+				backButton.Dispose ();
+				backButton = null;
+			}
+		}
+	}
 }
