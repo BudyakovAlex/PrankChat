432c2083044fc5e9b93b74130c6a62e43e817b68
Constant Shmelev <contact@constantshmelev.com>
2019-11-21 20:26:29 +0300

    [iOS] Add gradient navigation bar background, set light status bar, tab bar

--- src/iOS/AppTheme/Theme.cs
+++ src/iOS/AppTheme/Theme.cs
@@ -7,39 +7,43 @@ namespace PrankChat.Mobile.iOS.AppTheme
 	{
 		public class Color
 		{
-			public static UIColor Accent => UIColor.FromRGB(253, 222, 11);
-		}
+			public static UIColor White => UIColor.FromRGB(255, 255, 255);
+			public static UIColor Accent => UIColor.FromRGBA(0.427f, 0.157f, 0.745f, 1);
+			public static UIColor Inactive => UIColor.FromRGBA(0.129f, 0.129f, 0.129f, 1);
+			public static UIColor GradientHeaderStart => UIColor.FromRGBA(0.231f, 0.553f, 0.929f, 1);
+			public static UIColor GradientHeaderEnd => UIColor.FromRGBA(0.427f, 0.157f, 0.745f, 1);
+        }
 
 		public class Font
 		{
 			public static UIFont BlackOfSize(nfloat fontSize)
 			{
-				return UIFont.FromName("Roboto-Black", fontSize);
+				return UIFont.SystemFontOfSize(fontSize, UIFontWeight.Black);
 			}
 
 			public static UIFont BoldOfSize(nfloat fontSize)
 			{
-				return UIFont.FromName("Roboto-Bold", fontSize);
-			}
+                return UIFont.SystemFontOfSize(fontSize, UIFontWeight.Bold);
+            }
 
 			public static UIFont LightOfSize(nfloat fontSize)
 			{
-				return UIFont.FromName("Roboto-Light", fontSize);
+                return UIFont.SystemFontOfSize(fontSize, UIFontWeight.Light);
 			}
 
 			public static UIFont MediumOfSize(nfloat fontSize)
 			{
-				return UIFont.FromName("Roboto-Medium", fontSize);
+                return UIFont.SystemFontOfSize(fontSize, UIFontWeight.Medium);
 			}
 
 			public static UIFont RegularFontOfSize(nfloat fontSize)
 			{
-				return UIFont.FromName("Roboto-Regular", fontSize);
+                return UIFont.SystemFontOfSize(fontSize, UIFontWeight.Regular);
 			}
 
 			public static UIFont ThinFontOfSize(nfloat fontSize)
 			{
-				return UIFont.FromName("Roboto-Thin", fontSize);
+                return UIFont.SystemFontOfSize(fontSize, UIFontWeight.Thin);
 			}
 		}
 	}
