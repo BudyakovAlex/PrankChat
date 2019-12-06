dde877fd37dec4c87438552d038a818bbabea852
Anton Tereshko <formation7@outlook.com>
2019-12-06 20:39:46 +0300

    Create order cell for Android

---
+++ src/Core/Presentation/ViewModels/Order/Items/OrderItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmCross.Commands;
+
+namespace PrankChat.Mobile.Core.Presentation.ViewModels.Order.Items
+{
+    public class OrderItemViewModel : BaseItemViewModel
+    {
+        private DateTime _orderTime;
+
+        public string OrderTitle { get; }
+
+        public string ProfilePhotoUrl { get; }
+
+        public string TimeText => $"{_orderTime.Day} : {_orderTime.Hour} : {_orderTime.Minute}";
+
+        public string PriceText { get; }
+
+        public MvxAsyncCommand OpenDetailsOrderCommand => new MvxAsyncCommand(OnOpenDetailsOrderAsync);
+
+        public OrderItemViewModel()
+        {
+        }
+
+        private Task OnOpenDetailsOrderAsync()
+        {
+        }
+    }
+}
--- src/Core/Presentation/ViewModels/Order/OrdersViewModel.cs
+++ src/Core/Presentation/ViewModels/Order/OrdersViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using MvvmCross.ViewModels;
 using PrankChat.Mobile.Core.Presentation.Navigation;
+using PrankChat.Mobile.Core.Presentation.ViewModels.Publication.Items;
 
 namespace PrankChat.Mobile.Core.Presentation.ViewModels.Order
 {
     public class OrdersViewModel : BaseViewModel
     {
+        public MvxObservableCollection<PublicationItemViewModel> Items { get; } = new MvxObservableCollection<PublicationItemViewModel>();
+
         public OrdersViewModel(INavigationService navigationService) : base(navigationService)
         {
+            Items.Add(new PublicationItemViewModel());
+            Items.Add(new PublicationItemViewModel());
         }
     }
 }
