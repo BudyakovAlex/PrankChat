5de993f881eeb6dc82f3435ad646ad113ad672e5
khasenevich <v.khasenevich@pushnovn.com>
2020-01-23 16:51:02 +0300

    update extensions

--- src/Core/Infrastructure/Extensions/CountExtensions.cs
+++ src/Core/Infrastructure/Extensions/CountExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using PrankChat.Mobile.Core.Presentation.Localization;
 
 namespace PrankChat.Mobile.Core.Infrastructure.Extensions
@@ -10,6 +9,14 @@ namespace PrankChat.Mobile.Core.Infrastructure.Extensions
         private const string FormatForCountWithFraction = "#,0";
         private const double BigLimitForCount = 1000000D;
         private const double SmallLimitForCount = 1000D;
+        private const string DefaultValueIfNull = "0";
+
+        public static string ToCountString(this int? count)
+        {
+            if (count.HasValue)
+                return count.Value.ToCountString();
+            return DefaultValueIfNull;
+        }
 
         public static string ToCountString(this int count)
         {
@@ -23,6 +30,13 @@ namespace PrankChat.Mobile.Core.Infrastructure.Extensions
             if (count >= SmallLimitForCount)
                 return (count / SmallLimitForCount).ToString(FormatForCount) + Resources.Count_Thousand;
             return count.ToString(FormatForCountWithFraction);
+        }
+
+        public static string ToCountViewsString(this long? count)
+        {
+            if (count.HasValue)
+                return count.Value.ToCountViewsString();
+            return DefaultValueIfNull;
         }
 
         public static string ToCountViewsString(this long count)
--- src/Core/Infrastructure/Extensions/PriceExtensions.cs
+++ src/Core/Infrastructure/Extensions/PriceExtensions.cs
@@ -5,10 +5,23 @@ namespace PrankChat.Mobile.Core.Infrastructure.Extensions
     public static class PriceExtensions
     {
         private const string FormatForPrice = "#,#.#";
+        private const string DefaultValueIfNull = "0";
+
+        public static string ToPriceString(this double? price)
+        {
+            if (price.HasValue)
+                return price.Value.ToPriceString();
+            return GetStringWithCurrency(DefaultValueIfNull);
+        }
 
         public static string ToPriceString(this double price)
         {
-            return price.ToString($"{FormatForPrice} {Resources.Currency}").Replace(',', ' ');
+            return price.ToString(GetStringWithCurrency(FormatForPrice)).Replace(',', ' ');
+        }
+
+        private static string GetStringWithCurrency(string value)
+        {
+            return $"{value} {Resources.Currency}";
         }
     }
 }
