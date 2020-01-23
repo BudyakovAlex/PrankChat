5967c5fdfa551bda20207ccf7c32a2930bd1c56d
khasenevich <v.khasenevich@pushnovn.com>
2020-01-23 16:48:38 +0300

    update user api and data model

--- src/Core/Models/Data/UserDataModel.cs
+++ src/Core/Models/Data/UserDataModel.cs
@@ -2,16 +2,26 @@
 {
     public class UserDataModel
     {
-        public string id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public string Email { get; set; }
-
         public string Login { get; set; }
 
-        public double Balance { get; set; }
+        public string Email { get; set; }
+
+        public double? Balance { get; set; }
 
         public string Avatar { get; set; }
+
+        public int? OrdersOwnCount { get; set; }
+
+        public int? OrdersExecuteCount { get; set; }
+
+        public int? OrdersExecuteFinishedCount { get; set; }
+
+        public int? SubscribersCount { get; set; }
+
+        public int? SubscriptionsCount { get; set; }
     }
 }
