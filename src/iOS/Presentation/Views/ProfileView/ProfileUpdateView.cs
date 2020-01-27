3115bcb6721b3a43d9c6a644ccc60c1cbd94fa2e
vlad <v.khasenevich@pushnovn.com>
2020-01-27 16:21:55 +0300

    add ProfileUpdateView to ios project

---
+++ src/iOS/Presentation/Views/ProfileView/ProfileUpdateView.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using CoreGraphics;
+using MvvmCross.Binding;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Commands;
+using MvvmCross.Platforms.Ios.Binding;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using PrankChat.Mobile.Core.Converters;
+using PrankChat.Mobile.Core.Models.Enums;
+using PrankChat.Mobile.Core.Presentation.Localization;
+using PrankChat.Mobile.Core.Presentation.ViewModels.Profile;
+using PrankChat.Mobile.iOS.AppTheme;
+using PrankChat.Mobile.iOS.Presentation.Views.Base;
+using UIKit;
+
+namespace PrankChat.Mobile.iOS.Presentation.Views.ProfileView
+{
+    [MvxModalPresentation(WrapInNavigationController = true)]
+    public partial class ProfileUpdateView : BaseTransparentBarView<ProfileUpdateViewModel>
+    {
+        public IMvxAsyncCommand OpenCalendarCommand { get; set; }
+
+        public IMvxCommand<GenderType> SelectGenderCommand { get; set; }
+
+        private GenderType _gender;
+        public GenderType Gender
+        {
+            get => _gender;
+            set
+            {
+               
+            }
+        }
+
+        protected override void SetupBinding()
+        {
+            var set = this.CreateBindingSet<ProfileUpdateView, ProfileUpdateViewModel>();
+
+            set.Bind(emailTextField)
+                .For(v => v.Text)
+                .To(vm => vm.Email)
+                .Mode(MvxBindingMode.TwoWay);
+
+            set.Bind(loginTextField)
+                .For(v => v.Text)
+                .To(vm => vm.Login)
+                .Mode(MvxBindingMode.TwoWay);
+
+            set.Bind(nameTextField)
+                .For(v => v.Text)
+                .To(vm => vm.Name)
+                .Mode(MvxBindingMode.TwoWay);
+
+            set.Bind(birthdayTextField)
+                .For(v => v.Text)
+                .To(vm => vm.BirthdayText);
+
+            set.Bind(this)
+                .For(v => v.OpenCalendarCommand)
+                .To(vm => vm.SelectBirthdayCommand);
+
+            set.Bind(this)
+                .For(v => v.SelectGenderCommand)
+                .To(vm => vm.SelectGenderCommand);
+
+            set.Bind(progressBar)
+                .For(v => v.BindHidden())
+                .To(vm => vm.IsBusy)
+                .WithConversion<MvxInvertedBooleanConverter>();
+
+            set.Bind(saveButton)
+                .To(vm => vm.ProfileUpdateCommand);
+
+            set.Apply();
+        }
+
+        protected override void SetupControls()
+        {
+            this.View.BackgroundColor = UIColor.Black;
+
+            Title = Resources.ProfileUpdateView_Title;
+
+            emailTextField.SetLightStyle(Resources.ProfileUpdateView_Email_Placeholder);
+
+            loginTextField.SetLightStyle(Resources.ProfileUpdateView_Login_Placeholder);
+
+            nameTextField.SetLightStyle(Resources.ProfileUpdateView_Name_Placeholder);
+
+            birthdayTextField.SetLightStyle(Resources.ProfileUpdateView_Birthday_Placeholder);
+            var imageView = new UIImageView(UIImage.FromBundle("ic_calendar"));
+            var imageContainer = new UIView(new CGRect(0, 0, 35, 22));
+            imageContainer.ContentMode = UIViewContentMode.Center;
+            imageContainer.AddSubview(imageView);
+            birthdayTextField.RightView = imageContainer;
+            birthdayTextField.RightViewMode = UITextFieldViewMode.Always;
+            birthdayTextField.AddGestureRecognizer(new UITapGestureRecognizer(HandleCalendarTap));
+
+
+            sexSelectTitleLabel.Text = Resources.ProfileUpdateView_GenderSelect_Title;
+            sexSelectTitleLabel.TextColor = Theme.Color.White;
+            sexSelectTitleLabel.Font = Theme.Font.RegularFontOfSize(14);
+
+            maleTitleButton.SetTitle(Resources.RegistrationView_Male_Button, UIControlState.Normal);
+            maleTitleButton.AddTarget((s, e) => HandleRadioTap(GenderType.Male), UIControlEvent.TouchUpInside);
+            maleTitleButton.SetRadioTitleStyle();
+
+            maleIconButton.SetRadioInactiveStyle();
+            maleIconButton.AddTarget((s, e) => HandleRadioTap(GenderType.Male), UIControlEvent.TouchUpInside);
+
+            maleButtonsContainerView.AddGestureRecognizer(new UITapGestureRecognizer(s => HandleRadioTap(GenderType.Male)));
+
+            femaleTitleButton.SetTitle(Resources.RegistrationView_Female_Button, UIControlState.Normal);
+            femaleTitleButton.SetRadioTitleStyle();
+            femaleTitleButton.AddTarget((s, e) => HandleRadioTap(GenderType.Female), UIControlEvent.TouchUpInside);
+
+            femaleIconButton.SetRadioInactiveStyle();
+            femaleIconButton.AddTarget((s, e) => HandleRadioTap(GenderType.Female), UIControlEvent.TouchUpInside);
+
+            femaleButtonsContainerView.AddGestureRecognizer(new UITapGestureRecognizer(s => HandleRadioTap(GenderType.Female)));
+
+            saveButton.SetLightStyle(Resources.ProfileUpdateView_Button_Save);
+        }
+
+        protected override void RegisterKeyboardDismissResponders(List<UIView> views)
+        {
+            views.Add(scrollView);
+            base.RegisterKeyboardDismissResponders(views);
+        }
+
+        protected override void RegisterKeyboardDismissTextFields(List<UIView> viewList)
+        {
+            viewList.AddRange(new[] {
+                emailTextField,
+                loginTextField,
+                nameTextField,
+                birthdayTextField
+            });
+
+            base.RegisterKeyboardDismissTextFields(viewList);
+        }
+
+        private void HandleRadioTap(GenderType sex)
+        {
+            switch (sex)
+            {
+                case GenderType.Male:
+                    // Set male.
+                    femaleIconButton.SetRadioInactiveStyle();
+                    maleIconButton.SetRadioActiveStyle();
+                    break;
+
+                case GenderType.Female:
+                    // Set female.
+                    femaleIconButton.SetRadioActiveStyle();
+                    maleIconButton.SetRadioInactiveStyle();
+                    break;
+            }
+            SelectGenderCommand?.Execute(sex);
+        }
+
+        private void HandleCalendarTap(object sender)
+        {
+            OpenCalendarCommand?.Execute();
+        }
+    }
+}
+
---
+++ src/iOS/Presentation/Views/ProfileView/ProfileUpdateView.designer.cs
@@ -0,0 +1,146 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace PrankChat.Mobile.iOS.Presentation.Views.ProfileView
+{
+    [Register ("ProfileUpdateView")]
+    partial class ProfileUpdateView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField birthdayTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField emailTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView femaleButtonsContainerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton femaleIconButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton femaleTitleButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField loginTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView maleButtonsContainerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton maleIconButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton maleTitleButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField nameTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIActivityIndicatorView progressBar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton saveButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIScrollView scrollView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel sexSelectTitleLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (birthdayTextField != null) {
+                birthdayTextField.Dispose ();
+                birthdayTextField = null;
+            }
+
+            if (emailTextField != null) {
+                emailTextField.Dispose ();
+                emailTextField = null;
+            }
+
+            if (femaleButtonsContainerView != null) {
+                femaleButtonsContainerView.Dispose ();
+                femaleButtonsContainerView = null;
+            }
+
+            if (femaleIconButton != null) {
+                femaleIconButton.Dispose ();
+                femaleIconButton = null;
+            }
+
+            if (femaleTitleButton != null) {
+                femaleTitleButton.Dispose ();
+                femaleTitleButton = null;
+            }
+
+            if (loginTextField != null) {
+                loginTextField.Dispose ();
+                loginTextField = null;
+            }
+
+            if (maleButtonsContainerView != null) {
+                maleButtonsContainerView.Dispose ();
+                maleButtonsContainerView = null;
+            }
+
+            if (maleIconButton != null) {
+                maleIconButton.Dispose ();
+                maleIconButton = null;
+            }
+
+            if (maleTitleButton != null) {
+                maleTitleButton.Dispose ();
+                maleTitleButton = null;
+            }
+
+            if (nameTextField != null) {
+                nameTextField.Dispose ();
+                nameTextField = null;
+            }
+
+            if (progressBar != null) {
+                progressBar.Dispose ();
+                progressBar = null;
+            }
+
+            if (saveButton != null) {
+                saveButton.Dispose ();
+                saveButton = null;
+            }
+
+            if (scrollView != null) {
+                scrollView.Dispose ();
+                scrollView = null;
+            }
+
+            if (sexSelectTitleLabel != null) {
+                sexSelectTitleLabel.Dispose ();
+                sexSelectTitleLabel = null;
+            }
+        }
+    }
+}
