3921490de07f06700b41c8f5115baab06532b7bf
Anton Tereshko <formation7@outlook.com>
2020-02-10 15:38:37 +0300

    Add crashlistiecs to android

---
+++ src/Core/BusinessServices/Crashlytic/ICrashlyticsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PrankChat.Mobile.Core.BusinessServices.CrashlyticService
+{
+    public interface ICrashlyticsService
+    {
+		void TrackEvent(string message);
+
+		void TrackError(Exception exception);
+	}
+}
---
+++ src/Droid/PlatformBusinessServices/Crashlytics/CrashlyticsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PrankChat.Mobile.Core.BusinessServices.CrashlyticService;
+
+namespace PrankChat.Mobile.Droid.PlatformBusinessServices.Crashlytics
+{
+    public class CrashlyticsService : ICrashlyticsService
+    {
+		public void TrackEvent(string message)
+		{
+			Crashlytics.Crashlytics.Log(message);
+		}
+
+		public void TrackError(Exception exception)
+		{
+			if (exception is WebViewException ex)
+			{
+				if (ex.Code != 0)
+					Crashlytics.Crashlytics.SetInt("Error Code", ex.Code);
+
+				if (ex.Url != null)
+					Crashlytics.Crashlytics.SetString("Url", ex.Url);
+			}
+
+			Crashlytics.Crashlytics.LogException(Java.Lang.Throwable.FromException(exception));
+		}
+	}
+}
