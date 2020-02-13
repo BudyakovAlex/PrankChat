b438cff38c643e8fa9a9fd0b952c273781e62f58
Anton Tereshko <formation7@outlook.com>
2020-02-13 10:11:03 +0300

    Change json

--- src/Core/Models/Api/RefillApiData.cs
+++ src/Core/Models/Api/RefillApiData.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace PrankChat.Mobile.Core.Models.Api
 {
     public class RefillApiData
     {
+        [JsonProperty("amount")]
         public double Amount { get; set; }
     }
 }
