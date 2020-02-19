a4b42821f598a0ea6ef4b69dfd7980072b7d5761
Anton Tereshko <formation7@outlook.com>
2020-02-19 10:02:18 +0300

    Fix build

--- src/Droid/Presentation/Bindings/BackgroundBinding.cs
+++ src/Droid/Presentation/Bindings/BackgroundBinding.cs
@@ -3,16 +3,19 @@ using System.Reflection;
 using Android.Views;
 using MvvmCross.Binding;
 using MvvmCross.Binding.Bindings.Target;
+using MvvmCross.Platforms.Android.Binding.Target;
 
 namespace PrankChat.Mobile.Droid.Presentation.Bindings
 {
-    internal class BackgroundBinding : MvxPropertyInfoTargetBinding<View>
+    internal class BackgroundBinding : MvxAndroidTargetBinding
     {
         public static string TargetBinding = "Background";
 
+        public override Type TargetType => typeof(View);
+
         public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
 
-        public BackgroundBinding(object target, PropertyInfo targetPropertyInfo) : base(target, targetPropertyInfo)
+        public BackgroundBinding(object target) : base(target)
         {
         }
 
