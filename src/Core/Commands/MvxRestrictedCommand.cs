e5b18d094bb2bf3b98664924223d000d29471ae8
Alex Budyakov <a.budyakov@kdd-digital.de>
2020-02-22 18:54:29 +0200

    wip

--- src/Core/Commands/MvxRestrictedCommand.cs
+++ src/Core/Commands/MvxRestrictedCommand.cs
@@ -5,22 +5,22 @@ using MvvmCross.Commands;
 
 namespace PrankChat.Mobile.Core.Commands
 {
-    public class MvxRestrictedCommand : MvxCommandBase, IMvxCommand, ICommand
+	public class MvxRestrictedCommand : MvxCommandBase, IMvxCommand, ICommand
 	{
 		private readonly Func<bool> _canExecute;
-        private readonly Func<bool> _restrictedExecute;
-        private readonly Func<Task> _handleFunc;
-        private readonly Action _execute;
+		private readonly Func<bool> _restrictedExecute;
+		private readonly Func<Task> _handleFunc;
+		private readonly Action _execute;
 
 		public MvxRestrictedCommand(Action execute,
-                                    Func<bool> canExecute = null,
-                                    Func<bool> restrictedExecute = null,
+									Func<bool> canExecute = null,
+									Func<bool> restrictedExecute = null,
 									Func<Task> handleFunc = null)
 		{
 			_execute = execute;
 			_canExecute = canExecute;
-            _restrictedExecute = restrictedExecute;
-            _handleFunc = handleFunc;
+			_restrictedExecute = restrictedExecute;
+			_handleFunc = handleFunc;
 		}
 
 		public bool CanExecute(object parameter)
@@ -42,7 +42,7 @@ namespace PrankChat.Mobile.Core.Commands
 
 			if (_restrictedExecute?.Invoke() ?? true)
 			{
-			    _execute.Invoke();
+				_execute.Invoke();
 				return;
 			}
 
@@ -51,7 +51,7 @@ namespace PrankChat.Mobile.Core.Commands
 				return;
 			}
 
-		    _handleFunc.Invoke();
+			_handleFunc.Invoke();
 		}
 
 		public void Execute()
