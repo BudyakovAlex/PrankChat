2b30ecccbd1a134f905dfacc310151a3dca30807
Alex Budyakov <a.budyakov@kdd-digital.de>
2020-02-22 18:56:37 +0200

    fix format

--- src/Core/Commands/MvxRestrictedCommand.cs
+++ src/Core/Commands/MvxRestrictedCommand.cs
@@ -5,58 +5,58 @@ using MvvmCross.Commands;
 
 namespace PrankChat.Mobile.Core.Commands
 {
-	public class MvxRestrictedCommand : MvxCommandBase, IMvxCommand, ICommand
-	{
-		private readonly Func<bool> _canExecute;
-		private readonly Func<bool> _restrictedExecute;
-		private readonly Func<Task> _handleFunc;
-		private readonly Action _execute;
+    public class MvxRestrictedCommand : MvxCommandBase, IMvxCommand, ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Func<bool> _restrictedExecute;
+        private readonly Func<Task> _handleFunc;
+        private readonly Action _execute;
 
-		public MvxRestrictedCommand(Action execute,
-									Func<bool> canExecute = null,
-									Func<bool> restrictedExecute = null,
-									Func<Task> handleFunc = null)
-		{
-			_execute = execute;
-			_canExecute = canExecute;
-			_restrictedExecute = restrictedExecute;
-			_handleFunc = handleFunc;
-		}
+        public MvxRestrictedCommand(Action execute,
+            Func<bool> canExecute = null,
+            Func<bool> restrictedExecute = null,
+            Func<Task> handleFunc = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+            _restrictedExecute = restrictedExecute;
+            _handleFunc = handleFunc;
+        }
 
-		public bool CanExecute(object parameter)
-		{
-			 return _canExecute?.Invoke() ?? true;
-		}
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
 
-		public bool CanExecute()
-		{
-			return CanExecute(null);
-		}
+        public bool CanExecute()
+        {
+            return CanExecute(null);
+        }
 
-		public void Execute(object parameter)
-		{
-			if (!CanExecute(parameter))
-			{
-				return;
-			}
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
 
-			if (_restrictedExecute?.Invoke() ?? true)
-			{
-				_execute.Invoke();
-				return;
-			}
+            if (_restrictedExecute?.Invoke() ?? true)
+            {
+                _execute.Invoke();
+                return;
+            }
 
-			if (_handleFunc is null)
-			{
-				return;
-			}
+            if (_handleFunc is null)
+            {
+                return;
+            }
 
-			_handleFunc.Invoke();
-		}
+            _handleFunc.Invoke();
+        }
 
-		public void Execute()
-		{
-			Execute(null);
-		}
-	}
+        public void Execute()
+        {
+            Execute(null);
+        }
+    }
 }
