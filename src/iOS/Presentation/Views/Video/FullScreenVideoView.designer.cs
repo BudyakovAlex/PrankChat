d0e14da2f62cd5d0f09fb7a57db17555094b2266
Alex <>
2020-02-24 13:55:58 +0100

    154 increased watch indicator touch area and moved views to xib iOS

--- src/iOS/Presentation/Views/Video/FullScreenVideoView.designer.cs
+++ src/iOS/Presentation/Views/Video/FullScreenVideoView.designer.cs
@@ -1,21 +1,130 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace PrankChat.Mobile.iOS.Presentation.Views.Video
 {
-    [Register("FullScreenVideoView")]
-    partial class FullScreenVideoView
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("FullScreenVideoView")]
+	partial class FullScreenVideoView
+	{
+		[Outlet]
+		UIKit.UIButton closeButton { get; set; }
+
+		[Outlet]
+		UIKit.UILabel descriptionLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView loadProgressView { get; set; }
+
+		[Outlet]
+		UIKit.NSLayoutConstraint loadProgressViewWidthConstraint { get; set; }
+
+		[Outlet]
+		UIKit.UIButton muteButton { get; set; }
+
+		[Outlet]
+		UIKit.UIView overlayView { get; set; }
+
+		[Outlet]
+		UIKit.UIButton playButton { get; set; }
+
+		[Outlet]
+		UIKit.UIView progressView { get; set; }
+
+		[Outlet]
+		UIKit.UILabel timeLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel titleLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView watchProgressControl { get; set; }
+
+		[Outlet]
+		UIKit.UIView watchProgressControlContainer { get; set; }
+
+		[Outlet]
+		UIKit.UIView watchProgressView { get; set; }
+
+		[Outlet]
+		UIKit.NSLayoutConstraint watchProgressViewWidthConstraint { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (watchProgressViewWidthConstraint != null) {
+				watchProgressViewWidthConstraint.Dispose ();
+				watchProgressViewWidthConstraint = null;
+			}
+
+			if (loadProgressViewWidthConstraint != null) {
+				loadProgressViewWidthConstraint.Dispose ();
+				loadProgressViewWidthConstraint = null;
+			}
+
+			if (closeButton != null) {
+				closeButton.Dispose ();
+				closeButton = null;
+			}
+
+			if (descriptionLabel != null) {
+				descriptionLabel.Dispose ();
+				descriptionLabel = null;
+			}
+
+			if (loadProgressView != null) {
+				loadProgressView.Dispose ();
+				loadProgressView = null;
+			}
+
+			if (muteButton != null) {
+				muteButton.Dispose ();
+				muteButton = null;
+			}
+
+			if (overlayView != null) {
+				overlayView.Dispose ();
+				overlayView = null;
+			}
+
+			if (playButton != null) {
+				playButton.Dispose ();
+				playButton = null;
+			}
+
+			if (progressView != null) {
+				progressView.Dispose ();
+				progressView = null;
+			}
+
+			if (timeLabel != null) {
+				timeLabel.Dispose ();
+				timeLabel = null;
+			}
+
+			if (titleLabel != null) {
+				titleLabel.Dispose ();
+				titleLabel = null;
+			}
+
+			if (watchProgressControl != null) {
+				watchProgressControl.Dispose ();
+				watchProgressControl = null;
+			}
+
+			if (watchProgressControlContainer != null) {
+				watchProgressControlContainer.Dispose ();
+				watchProgressControlContainer = null;
+			}
+
+			if (watchProgressView != null) {
+				watchProgressView.Dispose ();
+				watchProgressView = null;
+			}
+		}
+	}
 }
