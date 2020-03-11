684359260eb15fdd9e071a10dabd1449894152ba
vlad khasenevich <bender9642459@gmail.com>
2020-03-11 13:39:25 +0300

    add new default user icon, change PlaceholderImageConverter and create NotificationTextView

---
+++ src/Droid/Controls/NotificationTextView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+
+namespace PrankChat.Mobile.Droid.Controls
+{
+    public class NotificationTextView : TextView
+    {
+        public NotificationTextView(Context context) : base(context)
+        {
+        }
+
+        public NotificationTextView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public NotificationTextView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        public NotificationTextView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
+        {
+        }
+
+        protected NotificationTextView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+    }
+}
