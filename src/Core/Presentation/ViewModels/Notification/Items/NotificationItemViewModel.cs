04cdf44e45c1455b5a81cf499726ee6a751279f4
vlad khasenevich <bender9642459@gmail.com>
2020-03-12 12:25:01 +0300

    update cell_notification

--- src/Core/Presentation/ViewModels/Notification/Items/NotificationItemViewModel.cs
+++ src/Core/Presentation/ViewModels/Notification/Items/NotificationItemViewModel.cs
@@ -15,7 +15,6 @@ namespace PrankChat.Mobile.Core.Presentation.ViewModels.Notification.Items
         private readonly INavigationService _navigationService;
 
         private NotificationType? _notificationType;
-        private bool _isDelivered;
         private int? _userId;
 
         public string ProfileName { get; }
@@ -28,7 +27,7 @@ namespace PrankChat.Mobile.Core.Presentation.ViewModels.Notification.Items
 
         public string DateText { get; }
 
-        public string Status => _isDelivered ? Resources.NotificationStatus_Viewed : Resources.NotificationStatus_NotViewed;
+        public bool IsDelivered { get; }
 
         public string Title { get; }
 
@@ -48,7 +47,7 @@ namespace PrankChat.Mobile.Core.Presentation.ViewModels.Notification.Items
             Description = description;
             DateText = createdAt?.ToTimeAgoCommentString();
 
-            _isDelivered = isDelivered ?? false;
+            IsDelivered = isDelivered ?? false;
             _notificationType = type;
 
             switch (_notificationType)
