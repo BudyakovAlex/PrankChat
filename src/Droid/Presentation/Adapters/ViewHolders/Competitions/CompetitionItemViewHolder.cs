b77d7f9bbc77d5fdecf14ea80d95f2d099c2152f
Alex Budyakov <a.budyakov@kdd-digital.de>
2020-03-21 09:53:28 +0200

    added UI layouts

---
+++ src/Droid/Presentation/Adapters/ViewHolders/Competitions/CompetitionItemViewHolder.cs
@@ -0,0 +1,58 @@
+﻿using Android.Views;
+using Android.Widget;
+using FFImageLoading.Cross;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using PrankChat.Mobile.Droid.Presentation.Adapters.ViewHolders.Abstract;
+
+namespace PrankChat.Mobile.Droid.Presentation.Adapters.ViewHolders.Competitions
+{
+    public class CompetitionItemViewHolder : CardViewHolder
+    {
+        private TextView _titleTextView;
+        private TextView _descriptionTextView;
+        private TextView _termTitle;
+        private TextView _termTimerTextView;
+        private TextView _daysTextView;
+        private TextView _hoursTextView;
+        private TextView _minutesTextView;
+        private TextView _prizeTitleTextView;
+        private TextView _prizeTextView;
+        private TextView _numberTextView;
+        private TextView _likesTextView;
+        private ImageView _likesImageView;
+        private MvxCachedImageView _placeholderImageView;
+        private Button _actionButton;
+
+        public CompetitionItemViewHolder(View view, IMvxAndroidBindingContext context) : base(view, context)
+        {
+        }
+
+        protected override void DoInit(View view)
+        {
+            base.DoInit(view);
+
+            _titleTextView = view.FindViewById<TextView>(Resource.Id.title_text_view);
+            _descriptionTextView = view.FindViewById<TextView>(Resource.Id.description_text_view);
+            _termTitle = view.FindViewById<TextView>(Resource.Id.term_title_text_view);
+            _termTimerTextView = view.FindViewById<TextView>(Resource.Id.term_timer_text_view);
+            _daysTextView = view.FindViewById<TextView>(Resource.Id.time_days_text_view);
+            _hoursTextView = view.FindViewById<TextView>(Resource.Id.time_hours_text_view);
+            _minutesTextView = view.FindViewById<TextView>(Resource.Id.time_minutes_text_view);
+            _prizeTitleTextView = view.FindViewById<TextView>(Resource.Id.prize_title_text_view);
+            _prizeTextView = view.FindViewById<TextView>(Resource.Id.prize_text_view);
+            _numberTextView = view.FindViewById<TextView>(Resource.Id.number_text_view);
+            _likesTextView = view.FindViewById<TextView>(Resource.Id.likes_text_view);
+            _likesImageView = view.FindViewById<ImageView>(Resource.Id.likes_image_view);
+            _placeholderImageView = view.FindViewById<MvxCachedImageView>(Resource.Id.placeholder_image_view);
+            _actionButton = view.FindViewById<Button>(Resource.Id.action_button);
+        }
+
+        public override void BindData()
+        {
+            base.BindData();
+
+            var bindingSet = this.CreateBindingSet<CompetitionItemViewHolder, CompetitionItemViewModel>();
+        }
+    }
+}
---
+++ src/Droid/Presentation/Views/Competitions/CompetitionsView.cs
@@ -0,0 +1,29 @@
+﻿using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using PrankChat.Mobile.Core.Presentation.ViewModels;
+using PrankChat.Mobile.Droid.Presentation.Views.Base;
+
+namespace PrankChat.Mobile.Droid.Presentation.Views.Competitions
+{
+    [MvxTabLayoutPresentation(TabLayoutResourceId = Resource.Id.tabs, ViewPagerResourceId = Resource.Id.viewpager, ActivityHostViewModelType = typeof(MainViewModel))]
+    [Register(nameof(CompetitionsView))]
+    public class CompetitionsView : BaseTabFragment<CompetitionsViewModel>
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
