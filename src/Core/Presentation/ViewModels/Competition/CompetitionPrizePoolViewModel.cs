13a8cb9244eceb105d44405974bc2f1adb254eaf
Alex Budyakov <a.budyakov@kdd-digital.de>
2020-03-24 22:44:23 +0200

    fixed view models

--- src/Core/Presentation/ViewModels/Competition/CompetitionPrizePoolViewModel.cs
+++ src/Core/Presentation/ViewModels/Competition/CompetitionPrizePoolViewModel.cs
@@ -1,14 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using PrankChat.Mobile.Core.ApplicationServices.Dialogs;
+using PrankChat.Mobile.Core.ApplicationServices.ErrorHandling;
+using PrankChat.Mobile.Core.ApplicationServices.Network;
+using PrankChat.Mobile.Core.ApplicationServices.Settings;
+using PrankChat.Mobile.Core.Presentation.Navigation;
+using PrankChat.Mobile.Core.Presentation.ViewModels.Base;
 using PrankChat.Mobile.Core.Presentation.ViewModels.Competition.Items;
 
 namespace PrankChat.Mobile.Core.Presentation.ViewModels.Competition
 {
-    public class CompetitionPrizePoolViewModel
+    public class CompetitionPrizePoolViewModel : BaseViewModel
     {
-        public double PrizePool { get; set; }
-        public ObservableCollection<CompetitionPrizePoolItemViewModel>  competitionPrizePoolsList { get; set; }
+        public CompetitionPrizePoolViewModel(INavigationService navigationService,
+                                             IErrorHandleService errorHandleService,
+                                             IApiService apiService,
+                                             IDialogService dialogService,
+                                             ISettingsService settingsService) : base(navigationService, errorHandleService, apiService, dialogService, settingsService)
+        {
+            Items = new ObservableCollection<CompetitionPrizePoolItemViewModel>();
+        }
+
+        public double PrizePool { get; private set; }
+
+        public ObservableCollection<CompetitionPrizePoolItemViewModel> Items { get; }
+
+        //TODO: add loading logic here
+        public override Task Initialize()
+        {
+            PrizePool = 1000000;
+
+            RaisePropertyChanged(nameof(PrizePool));
+            return base.Initialize();
+        }
     }
 }
--- src/Core/Presentation/ViewModels/Competition/CompetitionRulesViewModel.cs
+++ src/Core/Presentation/ViewModels/Competition/CompetitionRulesViewModel.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MvvmCross.Plugin.Messenger;
-using MvvmCross.ViewModels;
+﻿using MvvmCross.ViewModels;
+using PrankChat.Mobile.Core.ApplicationServices.Dialogs;
+using PrankChat.Mobile.Core.ApplicationServices.ErrorHandling;
+using PrankChat.Mobile.Core.ApplicationServices.Network;
+using PrankChat.Mobile.Core.ApplicationServices.Settings;
+using PrankChat.Mobile.Core.Presentation.Navigation;
 using PrankChat.Mobile.Core.Presentation.ViewModels.Base;
 
 namespace PrankChat.Mobile.Core.Presentation.ViewModels.Competition
 {
-    public class CompetitionRulesViewModel
+    public class CompetitionRulesViewModel : BaseViewModel, IMvxViewModel<string>
     {
-        public string HtmlContent { get; }
+        public CompetitionRulesViewModel(INavigationService navigationService,
+                                         IErrorHandleService errorHandleService,
+                                         IApiService apiService,
+                                         IDialogService dialogService,
+                                         ISettingsService settingsService) : base(navigationService, errorHandleService, apiService, dialogService, settingsService)
+        {
+        }
+
+        public string HtmlContent { get; private set; }
+
+        public void Prepare(string parameter)
+        {
+            HtmlContent = parameter;
+        }
     }
 }
