3b84da9df402621f88e435d7ccc9f6f2dbb02819
Farrell_28 <vadim_lost@outlook.com>
2020-03-24 20:50:25 +0200

    Added models Competitions

---
+++ src/Core/Presentation/ViewModels/Competition/CompetitionPrizePoolViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using PrankChat.Mobile.Core.Presentation.ViewModels.Competition.Items;
+
+namespace PrankChat.Mobile.Core.Presentation.ViewModels.Competition
+{
+    public class CompetitionPrizePoolViewModel
+    {
+        public double PrizePool { get; set; }
+        public ObservableCollection<CompetitionPrizePoolItemViewModel>  competitionPrizePoolsList { get; set; }
+    }
+}
---
+++ src/Core/Presentation/ViewModels/Competition/CompetitionRulesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Plugin.Messenger;
+using MvvmCross.ViewModels;
+using PrankChat.Mobile.Core.Presentation.ViewModels.Base;
+
+namespace PrankChat.Mobile.Core.Presentation.ViewModels.Competition
+{
+    public class CompetitionRulesViewModel
+    {
+        public string HtmlContent { get; }
+    }
+}
