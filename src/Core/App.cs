91579151a8dd65c35dc7e6fca20b9306381060bd
sorochan <windflavour@gmail.com>
2020-10-06 22:58:01 +0800

    add sentry test

--- src/Core/App.cs
+++ src/Core/App.cs
@@ -10,12 +10,14 @@ using PrankChat.Mobile.Core.ApplicationServices.Permissions;
 using PrankChat.Mobile.Core.ApplicationServices.Platforms;
 using PrankChat.Mobile.Core.ApplicationServices.Timer;
 using PrankChat.Mobile.Core.BusinessServices.Logger;
+using PrankChat.Mobile.Core.BusinessServices.Sentry;
 using PrankChat.Mobile.Core.BusinessServices.TaskSchedulers;
 using PrankChat.Mobile.Core.BusinessServices.TaskSchedulers.BackgroundTasks.SendLogs;
 using PrankChat.Mobile.Core.Configuration;
 using PrankChat.Mobile.Core.Presentation.Navigation;
 using PrankChat.Mobile.Core.Presentation.ViewModels.Base;
 using PrankChat.Mobile.Core.Providers;
+using Sentry;
 
 namespace PrankChat.Mobile.Core
 {
@@ -25,12 +27,19 @@ namespace PrankChat.Mobile.Core
 
         public override void Initialize()
         {
+
+            using (SentrySdk.Init("https://077bd6b9706f4247a2bf8cab4ac3bf44@o453054.ingest.sentry.io/5454419"))
+            {
+                throw null;
+            }
+
             InitializeMappings();
 
             Mvx.IoCProvider.ConstructAndRegisterSingleton<ITimerService, TimerService>();
             Mvx.IoCProvider.LazyConstructAndRegisterSingleton<INavigationService, NavigationService>();
             Mvx.IoCProvider.LazyConstructAndRegisterSingleton<IWalkthroughsProvider, WalkthroughsProvider>();
             Mvx.IoCProvider.ConstructAndRegisterSingleton<ILogger, Logger>();
+            Mvx.IoCProvider.ConstructAndRegisterSingleton<ISentryService, SentryService>();
             Mvx.IoCProvider.ConstructAndRegisterSingleton<ISendLogsBackgroundTask, SendLogsBackgroundTask>();
             Mvx.IoCProvider.ConstructAndRegisterSingleton<IBackgroundTaskScheduler, BackgroundTaskScheduler>();
             Mvx.IoCProvider.LazyConstructAndRegisterSingleton<IApiService, ApiService>();
