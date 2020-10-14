689bbde4703cd491cca10f5a69e2dbcbb03780ff
sorochan <windflavour@gmail.com>
2020-10-14 19:06:43 +0800

    fix notifi competition push click

--- src/Core/Models/Enums/OrderCategory.cs
+++ src/Core/Models/Enums/OrderCategory.cs
@@ -1,12 +1,14 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using PrankChat.Mobile.Core.ApplicationServices.Network.JsonSerializers.Converters;
 using System.Runtime.Serialization;
 
 namespace PrankChat.Mobile.Core.Models.Enums
 {
-    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(StringEnumJsonConverter))]
     public enum OrderCategory
     {
+        Unknown,
         [EnumMember(Value = "standard")]
         Standard,
         [EnumMember(Value = "competition")]
