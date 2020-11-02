0c2efb4d604e5db2e3f2b13ee4607034ee4a7248
sorochan <windflavour@gmail.com>
2020-11-02 09:00:42 -0600

    sentry fix

--- src/iOS/Main.cs
+++ src/iOS/Main.cs
@@ -1,4 +1,5 @@
 ﻿using UIKit;
+using Sentry;
 
 namespace PrankChat.Mobile.iOS
 {
@@ -7,9 +8,15 @@ namespace PrankChat.Mobile.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+
+            using (SentrySdk.Init("https://077bd6b9706f4247a2bf8cab4ac3bf44@o453054.ingest.sentry.io/5454419"))
+            {
+
+            }   
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+
         }
     }
 }
