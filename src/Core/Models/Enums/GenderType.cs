c17bd41d2d392ee131ec009fb1eed52d4364c564
sorochan <windflavour@gmail.com>
2020-12-10 16:24:07 +0800

    return unknown

--- src/Core/Models/Enums/GenderType.cs
+++ src/Core/Models/Enums/GenderType.cs
@@ -1,8 +1,14 @@
-﻿namespace PrankChat.Mobile.Core.Models.Enums
+﻿using System.Runtime.Serialization;
+
+namespace PrankChat.Mobile.Core.Models.Enums
 {
     public enum GenderType
     {
+        [EnumMember(Value = "unknown")]
+        Unknown,
+        [EnumMember(Value = "female")]
         Female,
+        [EnumMember(Value = "male")]
         Male,
     }
 }
