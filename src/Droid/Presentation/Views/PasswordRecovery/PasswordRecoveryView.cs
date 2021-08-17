a3a2249b3fb321770aa2e8e757167ca76587de02
iyong <iyong@DESKTOP-3LT8NRC>
2021-08-17 13:33:03 +0300

    action_bar_back_and_logo.xml fragment_password_recovery.xml fragment_profile.xml

--- src/Droid/Presentation/Views/PasswordRecovery/PasswordRecoveryView.cs
+++ src/Droid/Presentation/Views/PasswordRecovery/PasswordRecoveryView.cs
@@ -1,7 +1,15 @@
 ﻿using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.AppBar;
+using Google.Android.Material.Button;
+using Google.Android.Material.TextField;
+using MvvmCross.Platforms.Android.Binding;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Plugin.Visibility;
 using PrankChat.Mobile.Core.Presentation.ViewModels.PasswordRecovery;
 using PrankChat.Mobile.Core.Presentation.ViewModels.Registration;
+using PrankChat.Mobile.Droid.Controls;
 using PrankChat.Mobile.Droid.Presentation.Views.Base;
 
 namespace PrankChat.Mobile.Droid.Presentation.Views.PasswordRecovery
@@ -14,10 +22,37 @@ namespace PrankChat.Mobile.Droid.Presentation.Views.PasswordRecovery
     [Register(nameof(PasswordRecoveryView))]
     public class PasswordRecoveryView : BaseFragment<PasswordRecoveryViewModel>
     {
+        private ImageButton _imageButtonBack;
+        private TextInputEditText _textInputEmailEditTextField;
+        private MaterialButton _materialButtonRegistration;
+        private ProgressBar _progressBar;
+
         public PasswordRecoveryView() : base(Resource.Layout.fragment_password_recovery)
         {
         }
 
         protected override bool HasBackButton => true;
-	}
+
+        protected override void Bind()
+        {
+            base.Bind();
+            using var bindingSet = CreateBindingSet();
+
+            bindingSet.Bind(_imageButtonBack).For(v => v.BindClick()).To(vm => vm.CloseCommand);
+            bindingSet.Bind(_textInputEmailEditTextField).For(v => v.Text).To(vm => vm.Email);
+            bindingSet.Bind(_materialButtonRegistration).For(v => v.BindClick()).To(vm => vm.RecoverPasswordCommand);
+            bindingSet.Bind(_progressBar).For(v => v.BindVisible()).To(vm => vm.IsBusy);
+            bindingSet.Bind(_progressBar).For(v => v.Visibility).To(vm => vm.IsBusy)
+                .WithConversion<MvxVisibilityValueConverter>();
+        }
+
+        protected override void SetViewProperties(View view)
+        {
+            base.SetViewProperties(view);
+            _imageButtonBack = view.FindViewById<ImageButton>(Resource.Id.back_button);
+            _textInputEmailEditTextField = view.FindViewById<TextInputEditText>(Resource.Id.email_edit_text_field);
+            _materialButtonRegistration = view.FindViewById<MaterialButton>(Resource.Id.registration_button);
+            _progressBar = view.FindViewById<ProgressBar>(Resource.Id.progressBar);
+        }
+    }
 }
--- src/Droid/Presentation/Views/Profile/ProfileView.cs
+++ src/Droid/Presentation/Views/Profile/ProfileView.cs
@@ -1,8 +1,13 @@
 ﻿using Android.Runtime;
 using Android.Views;
+using Android.Widget;
+using AndroidX.ConstraintLayout.Widget;
 using AndroidX.RecyclerView.Widget;
+using Google.Android.Material.Button;
 using Google.Android.Material.Tabs;
 using MvvmCross.Binding.BindingContext;
+using MvvmCross.DroidX;
+using MvvmCross.Platforms.Android.Binding;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using PrankChat.Mobile.Core.Models.Enums;
@@ -13,6 +18,7 @@ using PrankChat.Mobile.Droid.Controls;
 using PrankChat.Mobile.Droid.Presentation.Adapters;
 using PrankChat.Mobile.Droid.Presentation.Adapters.TemplateSelectors;
 using PrankChat.Mobile.Droid.Presentation.Adapters.ViewHolders.Orders;
+using PrankChat.Mobile.Droid.Presentation.Converters;
 using PrankChat.Mobile.Droid.Presentation.Views.Base;
 
 namespace PrankChat.Mobile.Droid.Presentation.Views.Profile
@@ -24,6 +30,17 @@ namespace PrankChat.Mobile.Droid.Presentation.Views.Profile
         private EndlessRecyclerView _endlessRecyclerView;
         private LinearLayoutManager _layoutManager;
         private RecycleViewBindableAdapter _adapter;
+        private CircleCachedImageView _circleCachedImageView;
+        private TextView _textViewProfileName;
+        private TextView _textViewProfilePrice;
+        private TextView _textViewProfileSubscribersValue;
+        private TextView _textViewProfileSubscriptionsValue;
+        private TextView _textViewDescriptionTextView;
+        private ConstraintLayout _constraintLayoutSubscriptionsView;
+        private ConstraintLayout _constraintLayoutSubscribersView;
+        private MaterialButton _materialButtonProfileRefillButton;
+        private MaterialButton _materialButtonProfileViewWithdrawal;
+        private MvxSwipeRefreshLayout _mvxSwipeRefreshLayout;
 
         protected override string TitleActionBar => Core.Localization.Resources.Profile_Tab;
 
@@ -57,6 +74,22 @@ namespace PrankChat.Mobile.Droid.Presentation.Views.Profile
 
             bindingSet.Bind(_adapter).For(v => v.ItemsSource).To(vm => vm.Items);
             bindingSet.Bind(_endlessRecyclerView).For(v => v.LoadMoreItemsCommand).To(vm => vm.LoadMoreItemsCommand);
+            bindingSet.Bind(_circleCachedImageView).For(v => v.ImagePath).To(vm => vm.ProfilePhotoUrl);
+            bindingSet.Bind(_circleCachedImageView).For(v => v.PlaceholderText).To(vm => vm.ProfileShortName).OneTime();
+            bindingSet.Bind(_circleCachedImageView).For(v => v.BindClick()).To(vm => vm.ShowUpdateProfileCommand);
+            bindingSet.Bind(_textViewProfileName).For(v => v.Text).To(vm => vm.Login);
+            bindingSet.Bind(_textViewProfilePrice).For(v => v.Text).To(vm => vm.Price).OneWay();
+            bindingSet.Bind(_textViewProfileSubscribersValue).For(v => v.Text).To(vm => vm.SubscribersValue);
+            bindingSet.Bind(_constraintLayoutSubscriptionsView).For(v => v.BindClick()).To(vm => vm.ShowSubscriptionsCommand);
+            bindingSet.Bind(_textViewProfileSubscriptionsValue).For(v => v.BindClick()).To(vm => vm.SubscriptionsValue);
+            bindingSet.Bind(_materialButtonProfileRefillButton).For(v => v.BindClick()).To(vm => vm.ShowRefillCommand);
+            bindingSet.Bind(_materialButtonProfileViewWithdrawal).For(v => v.BindClick()).To(vm => vm.ShowWithdrawalCommand);
+            bindingSet.Bind(_constraintLayoutSubscribersView).For(v => v.BindClick()).To(vm => vm.ShowSubscribersCommand);
+            bindingSet.Bind(_textViewDescriptionTextView).For(v => v.BindClick()).To(vm => vm.Description);
+            bindingSet.Bind(_textViewDescriptionTextView).For(v => v.Visibility).To(vm => vm.HasDescription)
+                .WithConversion<BoolToGoneConverter>();
+            bindingSet.Bind(_mvxSwipeRefreshLayout).For(v => v.Refreshing).To(vm => vm.IsBusy);
+            bindingSet.Bind(_mvxSwipeRefreshLayout).For(v => v.RefreshCommand).To(vm => vm.LoadProfileCommand);
         }
 
         protected override void SetViewProperties(View view)
@@ -64,6 +97,18 @@ namespace PrankChat.Mobile.Droid.Presentation.Views.Profile
             base.SetViewProperties(view);
 
             _endlessRecyclerView = view.FindViewById<EndlessRecyclerView>(Resource.Id.profile_publication_recycler_view);
+
+            _circleCachedImageView = view.FindViewById<CircleCachedImageView>(Resource.Id.profile_photo);
+            _textViewProfileName = view.FindViewById<TextView>(Resource.Id.profile_name);
+            _textViewProfilePrice = view.FindViewById<TextView>(Resource.Id.profile_price);
+            _textViewProfileSubscribersValue = view.FindViewById<TextView>(Resource.Id.profile_subscribers_value);
+            _constraintLayoutSubscriptionsView = view.FindViewById<ConstraintLayout>(Resource.Id.subscriptions_view);
+            _textViewProfileSubscriptionsValue = view.FindViewById<TextView>(Resource.Id.profile_subscriptions_value);
+            _materialButtonProfileRefillButton = view.FindViewById<MaterialButton>(Resource.Id.profile_refill_button);
+            _materialButtonProfileViewWithdrawal = view.FindViewById<MaterialButton>(Resource.Id.profile_withdrawal_button);
+            _constraintLayoutSubscribersView = view.FindViewById<ConstraintLayout>(Resource.Id.subscribers_view);
+            _textViewDescriptionTextView = view.FindViewById<TextView>(Resource.Id.description_text_view);
+            _mvxSwipeRefreshLayout = view.FindViewById<MvxSwipeRefreshLayout>(Resource.Id.swipe_refresh_layout);
 
             _layoutManager = new LinearLayoutManager(Context, LinearLayoutManager.Vertical, false);
             _endlessRecyclerView.SetLayoutManager(_layoutManager);
