2d140fcb39e685e17f62c3a4b95dd13010b1c412
iyong <iyong@DESKTOP-3LT8NRC>
2021-08-20 14:54:04 +0300

    ShareDialog

--- src/Droid/Presentation/Dialogs/ShareDialog.cs
+++ src/Droid/Presentation/Dialogs/ShareDialog.cs
@@ -2,6 +2,9 @@
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Widget;
+using Google.Android.Material.Button;
+using MvvmCross.Platforms.Android.Binding;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views.Fragments;
@@ -13,6 +16,10 @@ namespace PrankChat.Mobile.Droid.Presentation.Dialogs
     [Register(nameof(ShareDialog))]
     public class ShareDialog : MvxDialogFragment<ShareDialogViewModel>
     {
+        private ImageButton _shareButton;
+        private ImageButton _copyLinkButton;
+        private ImageButton _shareInstButton;
+        private MaterialButton _closeButton;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -26,7 +33,18 @@ namespace PrankChat.Mobile.Droid.Presentation.Dialogs
             Dialog.Window.SetGravity(GravityFlags.Bottom);
 
             var view = this.BindingInflate(Resource.Layout.dialog_share, null);
+            Bind();
             return view;
+        }
+
+        private void Bind()
+        {
+            using var bindingSet = CreateBindingSet();
+
+            bindingSet.Bind(_shareButton).For(v => v.BindClick()).To(vm => vm.ShareCommand);
+            bindingSet.Bind(_copyLinkButton).For(v => v.BindClick()).To(vm => vm.CopyLinkCommand);
+            bindingSet.Bind(_shareInstButton).For(v => v.BindClick()).To(vm => vm.ShareToInstagramCommand);
+            bindingSet.Bind(_closeButton).For(v => v.BindClick()).To(vm => vm.CloseCommand);
         }
     }
 }
