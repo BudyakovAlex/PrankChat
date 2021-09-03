8b6833d019ccc17eea7e676e8e81e83e61048292
iyong <iyong@DESKTOP-3LT8NRC>
2021-09-03 14:42:17 +0300

    bugfix

---
+++ src/Core/ViewModels/Common/PaginationViewModelResult{TResult}.cs
@@ -0,0 +1,36 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrankChat.Mobile.Core.ViewModels.Common
+{
+    public abstract class PaginationViewModelResult<TResult> : PaginationViewModel, IMvxViewModelResult<TResult>
+    {
+        public PaginationViewModelResult(int paginationSize)
+            : base(paginationSize)
+        {
+
+        }
+
+        public TaskCompletionSource<object> CloseCompletionSource { get; set; } = new TaskCompletionSource<object>();
+        protected virtual TResult DefaultResult { get; }
+
+        protected override async Task CloseAsync(bool? isPlatform)
+        {
+            if (isPlatform == true)
+            {
+                return;
+            }
+
+            var isCloseConfirmed = await ConfirmPlatformCloseAsync().ConfigureAwait(false);
+            if (!isCloseConfirmed)
+            {
+                return;
+            }
+
+            await NavigationManager.CloseAsync(this, DefaultResult).ConfigureAwait(false);
+        }
+    }
+}
