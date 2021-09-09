f78db6d23f3d0d742706f39fb1af41476cbf7304
iyong <iyong@DESKTOP-3LT8NRC>
2021-09-09 10:07:29 +0300

    RaisePropertiesChanged has been wraped

--- src/Core/Presentation/ViewModels/Abstract/Items/BaseItemsPageViewModelResult{TItem,TResult}.cs
+++ src/Core/Presentation/ViewModels/Abstract/Items/BaseItemsPageViewModelResult{TItem,TResult}.cs
@@ -27,7 +27,10 @@ namespace PrankChat.Mobile.Core.Presentation.ViewModels.Abstract.Items
                 if (SetProperty(ref _items, value, OnItemsChanged))
                 {
                     _itemsSubscription.Disposable = value.WeakSubscribe(OnItemsCollectionChanged);
-                    RaisePropertiesChanged(nameof(Count), nameof(IsEmpty), nameof(IsNotEmpty));
+                    RaisePropertiesChanged(
+                        nameof(Count),
+                        nameof(IsEmpty),
+                        nameof(IsNotEmpty));
                 }
             }
         }
@@ -40,7 +43,10 @@ namespace PrankChat.Mobile.Core.Presentation.ViewModels.Abstract.Items
 
         private void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            RaisePropertiesChanged(nameof(Count), nameof(IsEmpty), nameof(IsNotEmpty));
+            RaisePropertiesChanged(
+                nameof(Count),
+                nameof(IsEmpty),
+                nameof(IsNotEmpty));
             OnItemsCollectionChanged();
         }
 
